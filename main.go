78dc28718aad846ea1fa6f8701feef736a367b96
whisky shusuky <whisky.shusuky@gmail.com>
2020-02-14 07:13:05 +0900

    feat(json): add nullable json.

--- main.go
+++ main.go
@@ -5,9 +5,21 @@ import "github.com/gin-gonic/gin"
 func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+
+		type NullableJson struct {
+			NullableIDInit    *int `json:nullable_id_init`
+			NullableIDNotInit *int `json:nullable_id_not_init`
+			NotNullIDInit     int  `json:not_null_id_init`
+			NotNullIDNotInit  int  `json:not_null_id_not_init`
+		}
+
+		nullableJson := new(NullableJson)
+
+		tmpInt := 1
+		nullableJson.NullableIDInit = &tmpInt
+		nullableJson.NotNullIDInit = 1
+
+		c.JSON(200, nullableJson)
 	})
 	r.Run()
 }
