de374c73a6e087f4bb0bb872782e3e28130bc1ce
whisky shusuky <whisky.shusuky@gmail.com>
2020-02-15 03:30:39 +0900

    fix(variables): fix name of variables and key of structure.

--- main.go
+++ main.go
@@ -21,19 +21,19 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 
-		type NullableJson struct {
-			NullableIDInit    *int `json:nullable_id_init`
-			NullableIDNotInit *int `json:nullable_id_not_init`
-			NotNullIDInit     int  `json:not_null_id_init`
-			NotNullIDNotInit  int  `json:not_null_id_not_init`
+		type NullableJSON struct {
+			NullableIntInit    *int `json:nullable_id_init`
+			NullableIntNotInit *int `json:nullable_id_not_init`
+			NotNullIntInit     int  `json:not_null_id_init`
+			NotNullIntNotInit  int  `json:not_null_id_not_init`
 		}
 
-		nullableJson := new(NullableJson)
+		nullableJSON := new(NullableJSON)
 
-		nullableJson.NullableIDInit = EvenMinute()
-		nullableJson.NotNullIDInit = 1
+		nullableJSON.NullableIntInit = EvenMinute()
+		nullableJSON.NotNullIntInit = 1
 
-		c.JSON(200, nullableJson)
+		c.JSON(200, nullableJSON)
 	})
 	r.Run()
 }
